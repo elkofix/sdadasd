bb9be03fa49d8797ea618164e6e77b0a98f4560d
elkofix <holasoygermamwow@gmail.com>
2024-10-22 15:39:37 -0500

    hola desde postman

--- chat/src/main/java/com/example/chatg3/chat/configs/WebSocketConfig.java
+++ chat/src/main/java/com/example/chatg3/chat/configs/WebSocketConfig.java
@@ -12,10 +12,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
     
 
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-connect")
-            .setAllowedOriginPatterns("*")
-            .withSockJS();
-
         registry.addEndpoint("/ws-connect-js")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
